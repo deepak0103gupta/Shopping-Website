b1035bcf96e5725dd736228070255d067bfe404f
unknown <you@example.com>
2024-02-19 12:50:47 +0530

    Initial commit: Add project files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <div >
+      <Navbar className='bg-slate-900' />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
